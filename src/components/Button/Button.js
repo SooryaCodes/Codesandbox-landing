edffe11fd68a95fb9250c45c7925e5690648658d
Soorya Kriz <sooryakriz111@gmail.com>
2021-05-15 01:49:38 +0530

    Hero Section

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ export default function Button({ px, py, classes ,text}) {
   return (
     <>
       <button
-        className={`${classes} bg-indigo-500 hover:bg-indigo-600 transition-colors duration-300	ease-in-out	font-mono px-${px} py-${py} text-white text-xs rounded-sm focus:outline-none  `}
+        className={`${classes} bg-indigo-500 hover:bg-indigo-600 transition-all transform hover:scale-110 duration-300	ease-in-out	font-mono px-${px} py-${py} text-white text-xs rounded-sm focus:outline-none  `}
       >
         {text}
       </button>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import Button from "../Button/Button";
 export default function Hero() {
   return (
     <div className="bg-dark text-white">
-      <div className="container mx-auto flex justify-center flex-col items-center text-center py-40 px-5">
+      <div className="container mx-auto flex justify-center flex-col items-center text-center py-28 md:py-40 px-5">
         <h1 className="font-bold font-mono text-4xl 	">
           Where teams build faster, together
         </h1>
@@ -14,32 +14,32 @@ export default function Hero() {
         </p>
         <div className="flex justify-between items-center py-20">
           <img
-            className="w-22 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 px-3"
+            className="w-12 md:w-22 px-2 md:px-3 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 "
             src="/images/react.svg"
             alt="react"
           />
           <img
-            className="w-22 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 px-3"
+            className="w-12 md:w-22 px-2 md:px-3 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 "
             src="/images/js.svg"
             alt="js"
           />
           <img
-            className="w-22 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 px-3"
+            className="w-12 md:w-22 px-2 md:px-3 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 "
             src="/images/vue.svg"
             alt="vue"
           />
           <img
-            className="w-22 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 px-3"
+            className="w-12 md:w-22 px-2 md:px-3 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 "
             src="/images/angular.svg"
             alt="angular"
           />
           <img
-            className="w-22 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 px-3"
+            className="w-12 md:w-22 px-2 md:px-3 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 "
             src="/images/html.svg"
             alt="html"
           />
           <img
-            className="w-22 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 px-3"
+            className="w-12 md:w-22 px-2 md:px-3 transition-all duration-300 ease-in-out cursor-pointer opacity-40 transform hover:scale-125 hover:opacity-100 "
             src="/images/more.svg"
             alt="more"
           />
